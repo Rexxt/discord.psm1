35e2d2177603be02f336568c1a516d3b1ffedbc0
Mizu <40831463+Rexxt@users.noreply.github.com>
2021-06-24 19:26:25 +0200

    Get-DiscordOnline() function

---
+++ discord.psm1
@@ -0,0 +1,11 @@
+function Get-DiscordOnline() {
+    # https://stackoverflow.com/questions/20259251/powershell-script-to-check-the-status-of-a-url
+
+    $req = Invoke-WebRequest -uri "https://discord.com/"
+
+    if ($req.StatusCode -eq 200) {
+        return $true
+    } else {
+        return $false
+    }
+}
