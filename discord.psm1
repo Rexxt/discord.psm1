2e4c8354f6255018a61cf1538c95bcbbf0423aa8
Mizu <40831463+Rexxt@users.noreply.github.com>
2021-06-24 19:26:50 +0200

    Removed whitespace

--- discord.psm1
+++ discord.psm1
@@ -1,8 +1,6 @@
 function Get-DiscordOnline() {
     # https://stackoverflow.com/questions/20259251/powershell-script-to-check-the-status-of-a-url
-
     $req = Invoke-WebRequest -uri "https://discord.com/"
-
     if ($req.StatusCode -eq 200) {
         return $true
     } else {
